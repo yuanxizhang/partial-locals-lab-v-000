fb4ce0f0461d75f7be2cc12bc52393fd901176cd
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-03-29 23:49:14 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,7 +21,11 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    if params[:search]
+      @students = Student.search(params[:search])
+    else
+      @students = Student.all
+    end
   end
 
   def student_params
